f97d416aa92048d9a04b9e8aba4f9c557a7a66e1
Laurin Trapp <laurin.mathis.trapp@proton.me>
2022-09-28 21:06:25 +0200

    Post processing kernels for the framebuffer

--- src/main/java/render/passes/framebuffers/Framebuffer.java
+++ src/main/java/render/passes/framebuffers/Framebuffer.java
@@ -1,17 +1,54 @@
 package main.java.render.passes.framebuffers;
 
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.GL_FLOAT;
+import static org.lwjgl.opengl.GL11.GL_LINEAR;
+import static org.lwjgl.opengl.GL11.GL_RGB;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_MAG_FILTER;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_MIN_FILTER;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_BYTE;
+import static org.lwjgl.opengl.GL11.glBindTexture;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glDisable;
+import static org.lwjgl.opengl.GL11.glDrawArrays;
+import static org.lwjgl.opengl.GL11.glGenTextures;
+import static org.lwjgl.opengl.GL11.glTexImage2D;
+import static org.lwjgl.opengl.GL11.glTexParameteri;
 import static org.lwjgl.opengl.GL15.GL_ARRAY_BUFFER;
 import static org.lwjgl.opengl.GL15.GL_DYNAMIC_READ;
 import static org.lwjgl.opengl.GL15.glBindBuffer;
 import static org.lwjgl.opengl.GL15.glBufferData;
 import static org.lwjgl.opengl.GL15.glGenBuffers;
 import static org.lwjgl.opengl.GL20.glEnableVertexAttribArray;
+import static org.lwjgl.opengl.GL20.glUniform2fv;
 import static org.lwjgl.opengl.GL20.glUseProgram;
 import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
-import static org.lwjgl.opengl.GL30.*;
-import static org.lwjgl.system.MemoryUtil.*;
+import static org.lwjgl.opengl.GL30.GL_COLOR_ATTACHMENT0;
+import static org.lwjgl.opengl.GL30.GL_DEPTH24_STENCIL8;
+import static org.lwjgl.opengl.GL30.GL_DEPTH_STENCIL_ATTACHMENT;
+import static org.lwjgl.opengl.GL30.GL_FRAMEBUFFER;
+import static org.lwjgl.opengl.GL30.GL_FRAMEBUFFER_COMPLETE;
+import static org.lwjgl.opengl.GL30.GL_RENDERBUFFER;
+import static org.lwjgl.opengl.GL30.glBindFramebuffer;
+import static org.lwjgl.opengl.GL30.glBindRenderbuffer;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glCheckFramebufferStatus;
+import static org.lwjgl.opengl.GL30.glDeleteFramebuffers;
+import static org.lwjgl.opengl.GL30.glDeleteRenderbuffers;
+import static org.lwjgl.opengl.GL30.glFramebufferRenderbuffer;
+import static org.lwjgl.opengl.GL30.glFramebufferTexture2D;
+import static org.lwjgl.opengl.GL30.glGenFramebuffers;
+import static org.lwjgl.opengl.GL30.glGenRenderbuffers;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
+import static org.lwjgl.opengl.GL30.glRenderbufferStorage;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
+import glm.vec._2.Vec2;
 import main.java.gui.Engine_Main;
 import main.java.shader.ShaderProgram;
 
@@ -20,6 +57,7 @@ public class Framebuffer {
 	private boolean init = false;
 	private int vao = 0, vbo = 0, fbo = 0, rbo = 0, texture = 0;
 	private ShaderProgram program;
+	private int uniformScreenSize = 0;
 	
 	public Framebuffer() {
 		init();
@@ -99,6 +137,7 @@ public class Framebuffer {
 	
 	private void initShader() {
 		program = new ShaderProgram("Framebuffer");
+		uniformScreenSize = program.getUniformLocation("screenSize");
 	}
 	
 	public void bindFbo() {
@@ -154,7 +193,6 @@ public class Framebuffer {
 		bindFbo();
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//		glEnable(GL_DEPTH_TEST);
 	}
 	
 	public void renderColorAttachments() {
@@ -169,7 +207,12 @@ public class Framebuffer {
 				glDisable(GL_DEPTH_TEST);
 				glBindTexture(GL_TEXTURE_2D, texture);
 				{
+					
+					glUniform2fv(uniformScreenSize, new Vec2(Engine_Main.windowWidth, Engine_Main.windowHeight).toFA_());
+					
+//					glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 					glDrawArrays(GL_TRIANGLES, 0, 6);
+//					glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 				}
 				glBindVertexArray(0);
 			}
